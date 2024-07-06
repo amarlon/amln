b95ed63e26380c13ce38e75bd236c5f1d3582a3b
Anaelmarlon Luzayamo <39515872+amarlon@users.noreply.github.com>
2024-07-06 10:59:56 -0400

    Update lang.js

--- App/Assets/js/lang.js
+++ App/Assets/js/lang.js
@@ -2,22 +2,22 @@
 var userLang = navigator.language || navigator.userLanguage; 
 //cookie if existe
 let cookieEnabled = navigator.cookieEnabled;
-var superLanguage='';
-var superLanBlock=''; //if cookie existe 
+let superLanguage='';
+let superLanBlock=''; //if cookie existe 
 
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
+const setCookie = (cname,cvalue,exdays) =>{
+    let d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
+    let expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
+const  getCookie =(cname) => {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
@@ -26,10 +26,12 @@ function getCookie(cname) {
         }
     }
     return "";
+
+    
 }
 
-function checkCookie(username) {
-    var user=getCookie(username);
+const  checkCookie = (username)  =>{
+    let user=getCookie(username);
     if (user != "") {
         alert("Welcome again " + user);
     } else {
